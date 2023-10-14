48fa329b344e2e0c47c5b0c295710aba892f67dd
Wander Gonzalez <wandergonzalez0@hotmail.com>
2023-10-14 15:45:52 -0400

    Start project 2 shapes shooting game

---
+++ project-2/Vec2.h
@@ -0,0 +1,13 @@
+class Vec2
+{
+public:
+    float x = 0;
+    float y = 0;
+
+    Vec2();
+    Vec2(float xin, float yin);
+
+    bool operator == (const Vec2& rhs) const;
+
+    Vec2 operator * (const float val) const;
+};
