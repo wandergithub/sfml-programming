00c770471cabb749ef5cb9c3435d1cfbc2bc5a7f
Wander Gonzalez <wandergonzalez0@hotmail.com>
2023-10-15 02:12:29 -0400

    Add Vec2 function definitions to Vec2.h header

--- project-2/Vec2.h
+++ project-2/Vec2.h
@@ -8,6 +8,18 @@ public:
     Vec2(float xin, float yin);
 
     bool operator == (const Vec2& rhs) const;
+    bool operator != (const Vec2& rhs) const;
 
+    Vec2 operator + (const Vec2& rhs) const;
+    Vec2 operator - (const Vec2& rhs) const;
     Vec2 operator * (const float val) const;
+    Vec2 operator / (const float val) const;
+
+    void operator += (const Vec2& rhs) const;
+    void operator -= (const Vec2& rhs) const;
+    void operator *= (const float val) const;
+    void operator /= (const float val) const;
+
+    void normalize(const Vec2& rhs) const;
+    double length(const Vec2& rhs) const;
 };
